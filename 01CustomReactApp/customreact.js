2ab1af65ee80dd174788a44650e5cb7487b6ea25
Arun Kumar <aruncode49@gmail.com>
2023-09-19 16:57:31 +0530

    01 - Version2

--- 01CustomReactApp/customreact.js
+++ 01CustomReactApp/customreact.js
@@ -1,8 +1,18 @@
 function customRender(reactElement, mainContainer) {
+  /* Version - 1
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.children;
   domElement.setAttribute("href", reactElement.props.href);
   domElement.setAttribute("target", reactElement.props.target);
+  mainContainer.appendChild(domElement);
+  */
+
+  //   Version - 2
+  const domElement = document.createElement(reactElement.type);
+  domElement.textContent = reactElement.children;
+  for (const prop in reactElement.props) {
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
   mainContainer.appendChild(domElement);
 }
 
