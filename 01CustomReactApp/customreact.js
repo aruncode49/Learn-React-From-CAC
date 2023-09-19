19bb96752691fe0f0e4f93ee34093071000c144d
Arun Kumar <aruncode49@gmail.com>
2023-09-19 16:49:34 +0530

    01 creating custom react library

---
+++ 01CustomReactApp/customreact.js
@@ -0,0 +1,20 @@
+function customRender(reactElement, mainContainer) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  mainContainer.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const mainContainer = document.getElementById("root");
+
+customRender(reactElement, mainContainer);
