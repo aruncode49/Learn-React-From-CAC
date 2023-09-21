df72797ce44a6e7434dd7afeb3193bc41f6b340c
Arun Kumar <aruncode49@gmail.com>
2023-09-21 15:49:26 +0530

    Created pass gen func with useCallback & useEffect

--- 04PasswordGenerator/src/App.jsx
+++ 04PasswordGenerator/src/App.jsx
@@ -1,4 +1,33 @@
+import { useState, useCallback, useEffect } from "react";
+
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // Password Generator
+  const passwordGenrator = useCallback(() => {
+    let pswrd = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*()_+-`~{}[]";
+
+    for (let i = 1; i <= length; i++) {
+      let charIndex = Math.floor(Math.random() * str.length + 1);
+
+      pswrd += str.charAt(charIndex);
+    }
+
+    setPassword(pswrd);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+
+  // useEffect
+  useEffect(() => {
+    passwordGenrator();
+  }, [length, numberAllowed, characterAllowed]);
+
   return <h1 className="text-green-500">Hello World</h1>;
 }
 
