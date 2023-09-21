aa270af14126fcada7abcd52e786a8e22dedd9b8
Arun Kumar <aruncode49@gmail.com>
2023-09-21 13:13:57 +0530

    Usestate Interview Question Part-1

--- 02CounterProject/src/App.jsx
+++ 02CounterProject/src/App.jsx
@@ -8,6 +8,9 @@ function App() {
   // increase value function
   function increaseValue() {
     if (counterVariable < 10) {
+      // UseState update the UI in batches or group so, because there is no change in any function call the value updation remain same as previous
+      setCounterVariable(counterVariable + 1);
+      setCounterVariable(counterVariable + 1);
       setCounterVariable(counterVariable + 1);
     }
   }
@@ -22,8 +25,12 @@ function App() {
   return (
     <>
       <h1>Counter - {counterVariable}</h1>
-      <button onClick={increaseValue}>&uarr; Increase Value </button>
-      <button onClick={decreaseValue}>&darr; Decrease Value </button>
+      <button onClick={increaseValue}>
+        &uarr; Increase Value ( {counterVariable} ){" "}
+      </button>
+      <button onClick={decreaseValue}>
+        &darr; Decrease Value ( {counterVariable} ){" "}
+      </button>
     </>
   );
 }
